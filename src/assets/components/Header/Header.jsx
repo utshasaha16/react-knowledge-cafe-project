942d080404cfe61d1d8b82684300afa92c6fc13c
utshasaha16 <utsharoy12345@gmail.com>
2024-10-21 21:43:18 +0600

    create fake data and use useEffect load json data

---
+++ src/assets/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-bold'>Knowledge Cafe</h1>            
+        </div>
+    );
+};
+
+export default Header;
